ada1ecec4345adfed6e395f213a7e6709f96e25a
devpablohenrique <devpablohenrique@gmail.com>
2022-04-26 14:53:44 -0300

    Finalizando a seção 06 referente a customizacao do Tailwind

---
+++ Curso_udemy/06_customizacao/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        'new-blue': '#243c5a',
+        gray: {
+          900: 'rgb(254,0,0)',
+        }
+      },
+      spacing: {
+        '50': '20rem'
+      },
+    },
+  },
+  plugins: [],
+}
